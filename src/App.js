1b15de211c00adbd1ee34efe1a496a0cd3713984
KHarrisDev <kharrisjr.dev@gmail.com>
2023-09-10 16:37:17 -0400

    updating app.js
    Building a static layout from scratch. Rendering items from an item list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// 1. When starting an react application, declare functional components that similate the structure of the application.
+// 2. Render static elements from a static array of data into the UI
+// * When rendering a list of items, create a parent component that contains <ul> element. Then create a component that contains the <li> elements. This will similate the map function rendering data into the UI.
+// * Conditionally add style to the element creating the UI by add a css object to the JavaScript mode
 
-function App() {
+const initialData = [
+  { 
+    id: 1,
+    description: "Go to work",
+    completed: true
+  },
+  {
+    id: 2,
+    description: "Do laundry",
+    completed: false
+  },
+  {
+    id: 3,
+    description: "Go to the mall",
+    completed: false
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Form />
+      <TaskList />
+      <Statistics />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Task List Application</h1>
     </div>
   );
 }
 
-export default App;
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>Please type a task to complete in the input box.</h3>
+    </div>
+  );
+}
+
+const TaskList = () => {
+  return (
+    <ul className="task-list">
+      {initialData.map((item) => (
+        <Task item={item} />
+      ))}
+    </ul>
+  );
+}
+
+const Task = ({item}) => {
+  return (
+    <li>
+      <span style={item.completed ? { textDecoration: "line-through"} : {}}>
+        {item.description}
+      </span>
+      <button>Del</button>
+    </li>
+  );
+}
+
+const Statistics = () => {
+  return (
+    <div>
+      <footer className="stats">
+        <em>You have x items on your list and you completed x task (x%).</em>
+      </footer>
+    </div>
+  );
+}
