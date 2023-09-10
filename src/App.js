1e39ed67c073f492c0ef7f1b9a79db6ed00a7d29
KHarrisDev <kharrisjr.dev@gmail.com>
2023-09-10 17:55:11 -0400

    Update App.js
    Handled the form submission and created a new item to eventually be rendered in the UI.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@
 //  * onSubmit() will pass the event object to the handler function that contains the value typed by user.
 //  * Test event by logging event to the console.
 
+// 4. Create controlled element to allow react to control elements instead of the DOM.
+//  * Create a piece of state for the element that you want to control. <input /> for example.
+//  * Set the value inside the element equal to the state. This value will be the value of the input element now meaning it can't be changed when typing.
+//  * Create onChange() to update the state of the input element with set function (ie. setDescription). 
+//  * Create an object that takes the value of the user input (mimic the dummy objects uesd to create the UI)
+import { useState } from "react";
+
 const initialData = [
   { 
     id: 1,
@@ -47,15 +54,33 @@ const Header = () => {
 }
 
 const Form = () => {
+  const [ description, setDescription] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    // console.log(e);
+
+    const newTaskItem = { id: Date.now(), description, completed: false};
+    console.log(newTaskItem);
+
+    setDescription("");
+  }
+
+  const handleDescription = (e) => {
+    // console.log(e); //targets the event
+    // console.log(e.target); //targets the element
+    // console.log(e.target.value); //targets the value
+    setDescription(e.target.value);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Please type a task to complete in the input box.</h3>
-      <input type="text" placeholder="Enter a task . . ."/>
+      <input 
+      type="text"
+       placeholder="Enter a task . . ." 
+       value={description}
+       onChange={handleDescription}/>
       <button>Add Task</button>
     </form>
   );
