8604a29160213560a1b5fbab78ffff435c44d28c
KHarrisDev <kharrisjr.dev@gmail.com>
2023-09-10 17:00:44 -0400

    Update App.css
    Building a form to handle submission.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // 1. When starting an react application, declare functional components that similate the structure of the application.
+
 // 2. Render static elements from a static array of data into the UI
 // * When rendering a list of items, create a parent component that contains <ul> element. Then create a component that contains the <li> elements. This will similate the map function rendering data into the UI.
-// * Conditionally add style to the element creating the UI by add a css object to the JavaScript mode
+// * Conditionally add style to the element creating the UI by add a css object to the JavaScript mode.
+
 // 3. Use the form element to submit data.
-//  * Use preventDefault() to prevent a refresh when pressing enter
-//  * onSubmit() will pass the event object to the handler function that contains the value typed by user
+//  * Use preventDefault() to prevent a refresh when pressing enter.
+//  * onSubmit() will pass the event object to the handler function that contains the value typed by user.
+//  * Test event by logging event to the console.
 
 const initialData = [
   { 
@@ -44,8 +47,13 @@ const Header = () => {
 }
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Please type a task to complete in the input box.</h3>
       <input type="text" placeholder="Enter a task . . ."/>
       <button>Add Task</button>
